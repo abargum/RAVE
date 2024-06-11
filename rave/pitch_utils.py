f6199219922ae88f4b9eaaab0cbecb3d722b9d4b
abargum <abargum@gmail.com>
2024-06-11 14:54:58 +0200

    return f0 for visuals

--- rave/pitch_utils.py
+++ rave/pitch_utils.py
@@ -99,22 +99,20 @@ def one_hot(a, num_classes: int):
     one_hot_batched = one_hot_flat.reshape(shape[0], shape[1], num_classes)
     return one_hot_batched
 
-def extract_utterance_log_f0(y, mult: float, sr: int, frame_len_samples: int, hop_len_samples: int, voiced_prob_cutoff: float=0.2):
-    f0_1 = get_pitch(y, frame_len_samples)
-    f0 = f0_1 * mult
+def extract_utterance_log_f0(y, sr: int, frame_len_samples: int, hop_len_samples: int, voiced_prob_cutoff: float=0.2):
+    f0 = get_pitch(y, frame_len_samples)
     f0[f0 == 0] = float('nan')
     log_f0 = torch.log(f0)
-    return log_f0, f0_1
+    return log_f0, f0
 
 def quantize_f0_norm(y, f0_median, f0_std, fs: int, win_length: int, hop_length: int):
-    utt_log_f0, f0 = extract_utterance_log_f0(y, 1.0, fs, win_length, hop_length)
+    utt_log_f0, f0 = extract_utterance_log_f0(y, fs, win_length, hop_length)
     log_f0_norm = ((utt_log_f0 - f0_median) / f0_std) / 4.0
     return log_f0_norm, f0
 
 def get_f0_norm(y, f0_median, f0_std, fs: int, win_length: int, hop_length: int, num_f0_bins: int=256):
     log_f0_norm, f0 = quantize_f0_norm(y, f0_median, f0_std, fs, win_length, hop_length)
     log_f0_norm += 0.5
-    log_f0_norm = log_f0_norm
     bins = torch.linspace(0, 1, num_f0_bins+1).to(y)
     f0_one_hot_idxs = torch.bucketize(log_f0_norm, bins, right=True) - 1
     f0_one_hot = one_hot(f0_one_hot_idxs, num_f0_bins+1)
@@ -122,7 +120,7 @@ def get_f0_norm(y, f0_median, f0_std, fs: int, win_length: int, hop_length: int,
     return f0_one_hot, log_f0_norm, f0
 
 def extract_f0_median_std(wav, fs: int, win_length: int, hop_length: int):
-    log_f0_vals, _ = extract_utterance_log_f0(wav, 1.0, fs, win_length, hop_length)
+    log_f0_vals, _ = extract_utterance_log_f0(wav, fs, win_length, hop_length)
     log_f0_vals = log_f0_vals[~torch.isnan(log_f0_vals)]
 
     log_f0_median = torch.median(log_f0_vals)
